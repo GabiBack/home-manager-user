3011aadcd4c486b22db3f2cb39eff2aaf559ae5b
Gabriela Knapik <s210964@student.uek.krakow.pl>
2023-02-13 19:43:52 +0100

    Initial commit

---
+++ src/main/java/com/example/homemanageruser/HomeManagerUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.homemanageruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeManagerUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeManagerUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homemanageruser/HomeManagerUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homemanageruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeManagerUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
